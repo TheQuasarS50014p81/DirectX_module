dacc3163a16253d7f7b6fe4b832c1bd7cd67d1a3
neisanmara <epicgamesunrealengine4@gmail.com>
2019-03-17 17:19:16 +0900

    dx11初期化

--- WIN.cpp
+++ WIN.cpp
@@ -14,9 +14,13 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR szStr, INT iC
 	g_pWin = new WIN;
 	if (g_pWin != NULL) {
 		if(SUCCEEDED(g_pWin->InitWindow(hInstance,0,0,WINDOW_WIDTH,
-			WINDOW_HEIGHT,APP_NAME))){
-			g_pWin->Run();
+			WINDOW_HEIGHT,APP_NAME)))
+		{
+			if (SUCCEEDED(g_pWin->InitD3D())) {
+				g_pWin->Run();
+			}
 		}
+		g_pWin->DestroyD3D();
 		delete g_pWin;
 	}
 	return 0;
@@ -87,4 +91,83 @@ void WIN::Run()
 		}
 	}
 	//exit application
+}
+
+HRESULT WIN::InitD3D()
+{
+	//デバイスとスワップチェーン
+	DXGI_SWAP_CHAIN_DESC sd;
+	ZeroMemory(&sd, sizeof(sd));
+	sd.BufferCount = 1;
+	sd.BufferDesc.Width = WINDOW_WIDTH;
+	sd.BufferDesc.Height = WINDOW_HEIGHT;
+	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	sd.BufferDesc.RefreshRate.Numerator = 60;
+	sd.BufferDesc.RefreshRate.Denominator = 1;
+	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	sd.OutputWindow = m_hWnd;
+	sd.SampleDesc.Count = 1;
+	sd.SampleDesc.Quality = 0;
+	sd.Windowed = TRUE;
+
+	D3D_FEATURE_LEVEL pFeatureLevels = D3D_FEATURE_LEVEL_11_0;
+	D3D_FEATURE_LEVEL *pFeatureLevel = NULL;
+
+	if (FAILED(D3D11CreateDeviceAndSwapChain(NULL,D3D_DRIVER_TYPE_HARDWARE, NULL,
+		0,&pFeatureLevels, 1,D3D11_SDK_VERSION,&sd,&m_pSwapChain,&m_pDevice,
+		pFeatureLevel, &m_pDeviceContext))){
+		return FALSE;
+	}
+	//レンダーターゲットビューの作成
+	ID3D11Texture2D *pBackBuffer;
+	m_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+	m_pDevice->CreateRenderTargetView(pBackBuffer, NULL, &m_pRenderTargetView);
+	SAFE_RELEASE(pBackBuffer);
+	//深度ステンシルバッファビューの作成
+	D3D11_TEXTURE2D_DESC descDepth;
+	descDepth.Width = WINDOW_WIDTH;
+	descDepth.Height = WINDOW_HEIGHT;
+	descDepth.MipLevels = 1;
+	descDepth.ArraySize = 1;
+	descDepth.Format = DXGI_FORMAT_D32_FLOAT;
+	descDepth.SampleDesc.Count = 1;
+	descDepth.SampleDesc.Quality = 0;
+	descDepth.Usage = D3D11_USAGE_DEFAULT;
+	descDepth.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+	descDepth.CPUAccessFlags = 0;
+	descDepth.MiscFlags = 0;
+	m_pDevice->CreateTexture2D(&descDepth, NULL, &m_pDepthStencil);
+	m_pDevice->CreateDepthStencilView(m_pDepthStencil, NULL, &m_pDepthStencilView);
+	//レンダーターゲットビューと深度ステンシルビューをパイプラインにバインド
+	m_pDeviceContext->OMSetRenderTargets(1, &m_pRenderTargetView, m_pDepthStencilView);
+	//ビューポートの設定
+	D3D11_VIEWPORT vp;
+	vp.Width = WINDOW_WIDTH;
+	vp.Height = WINDOW_HEIGHT;
+	vp.MinDepth = 0.0f;
+	vp.MaxDepth = 1.0f;
+	vp.TopLeftX = 0;
+	vp.TopLeftY = 0;
+	m_pDeviceContext->RSSetViewports(1, &vp);
+	//ラスタライズ設定
+	D3D11_RASTERIZER_DESC rdc;
+	ZeroMemory(&rdc, sizeof(rdc));
+	rdc.CullMode = D3D11_CULL_NONE;
+	rdc.FillMode = D3D11_FILL_SOLID;
+	ID3D11RasterizerState *pIr = NULL;
+	m_pDevice->CreateRasterizerState(&rdc, &pIr);
+	m_pDeviceContext->RSSetState(pIr);
+	SAFE_RELEASE(pIr);
+
+	return S_OK;
+}
+
+void WIN::DestroyD3D()
+{
+	SAFE_RELEASE(m_pSwapChain);
+	SAFE_RELEASE(m_pRenderTargetView);
+	SAFE_RELEASE(m_pDeviceContext);
+	SAFE_RELEASE(m_pDepthStencil);
+	SAFE_RELEASE(m_pDepthStencilView);
+	SAFE_RELEASE(m_pDevice);
 }
--- WIN.h
+++ WIN.h
@@ -1,10 +1,19 @@
 #pragma once
 #include <Windows.h>
+#include <d3d11.h>
+#include <D3DX10.h>
+#include <d3dx11.h>
+#include <d3dcompiler.h>
+#pragma comment(lib,"d3d11.lib")
+#pragma comment(lib,"d3dx10.lib")
+#pragma comment(lib,"d3dx11.lib")
+#pragma comment(lib,"d3dCompiler.lib")
 
 //定数定義
 #define WINDOW_WIDTH 640	//ウィンドウ幅
 #define WINDOW_HEIGHT 480	//ウィンドウ高さ
-#define APP_NAME	L"ウィンドウ作成"
+#define APP_NAME	L"DxirectX_Modules"
+#define SAFE_RELEASE(x) if(x){x->Release(); x = NULL;}
 
 //
 //
@@ -12,8 +21,16 @@
 class WIN{
 public:
 	HRESULT InitWindow(HINSTANCE, INT, INT, INT, INT, LPCWSTR);
+	HRESULT InitD3D();
 	LRESULT MsgProc(HWND, UINT, WPARAM, LPARAM);
 	void Run();
+	void DestroyD3D();
 
 	HWND m_hWnd;
+	ID3D11Device *m_pDevice;
+	ID3D11DeviceContext *m_pDeviceContext;
+	IDXGISwapChain *m_pSwapChain;
+	ID3D11RenderTargetView *m_pRenderTargetView;
+	ID3D11DepthStencilView *m_pDepthStencilView;
+	ID3D11Texture2D *m_pDepthStencil;
 };
